9021600750b91d9a8e9e26b6aba718750346bddf
Annemette M <annemettemygind@gmail.com>
2026-01-30 17:34:07 +0100

    Specifik router: forskellige html elementer alt efter URL

--- 01-Express-1/02-specifik-routing.js
+++ 01-Express-1/02-specifik-routing.js
@@ -1,14 +1,21 @@
-app.get("/", (req, res) => {
-res.setHeader('Content-Type', "text/html"); //ikke nødvendigt hvis res.send() blot sender en plain streng
-res.send("<h2>Hovedside</h2>");
-};
+const express = require("express");
+const app = express();
+const port = 3000;
+const nav =
+  '<a href="http://localhost:3000/forside">Velkomst</a><a href="http://localhost:3000/om">Til login</a><a href="http://localhost:3000/oversigt">Til oversigt</a>';
 
+app.get("/forside", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send('<h1 style="color: blue;">Velkommen til denne side</h1>' + nav);
+});
 app.get("/om", (req, res) => {
-res.setHeader('Content-Type', "text/html");
-res.send("<h2>Loginside</h2>");
-};
-
+  res.setHeader("Content-Type", "text/html");
+  res.send("<h2>Loginside</h2>" + nav);
+});
 app.get("/oversigt", (req, res) => {
-res.setHeader('Content-Type', "text/html");
-res.send("<h2>Oversigtsside<h2>");
-};
+  res.setHeader("Content-Type", "text/html");
+  res.send("<h2>Oversigtsside<h2>" + nav);
+});
+app.listen(port, () => {
+  console.log(`Serveren kører på http://localhost:${port}`);
+});
