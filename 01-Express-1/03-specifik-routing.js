465b8254e578090daeee425b74fbbdfa93005d15
Annemette M <annemettemygind@gmail.com>
2026-02-01 16:44:44 +0100

    Specifik routing med hele filer: .sendFile()

--- 01-Express-1/03-specifik-routing.js
+++ 01-Express-1/03-specifik-routing.js
@@ -3,19 +3,14 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/forside", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/forside.html"));
 });
-
-app.get("/index", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.sendFile(path.join(__dirname, "public", "om.html"));
+app.get("/om", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/om.html"));
 });
-
-app.get("/kontakt", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.sendFile(path.join(__dirname, "public", "kontakt.html"));
+app.get("/oversigt", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/oversigt.html"));
 });
 
 app.listen(port, () => {
