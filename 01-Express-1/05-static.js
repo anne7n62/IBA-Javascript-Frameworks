2986f0a59d7afc3da44c20c06413d68e102a37a2
Annemette M <annemettemygind@gmail.com>
2026-02-02 11:37:15 +0100

    Sender fuld hjemmeside med express.static();

--- 01-Express-1/05-static.js
+++ 01-Express-1/05-static.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-// const path = require("path");
 
-app.use(
-  express.static(path.join(__dirname, "public"), { extensions: ["html"] }),
-);
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`server kører på http://localhost:${port}`);
